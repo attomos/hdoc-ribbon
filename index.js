c933e024020889f0faebcb7465237ba547441d77
Nattaphoom Chaipreecha <attomos@gmail.com>
2022-01-22 16:49:16 +0700

    feat: support theming

--- index.js
+++ index.js
@@ -4,57 +4,22 @@ class Ribbon extends HTMLElement {
     // Create a shadow root
     const shadow = this.attachShadow({ mode: "open" });
 
+    // default theme
+    this._theme = {
+      borderColor: "#ababab",
+      backgroundColor: "#ffffff",
+      backgroundHoverColor: "#dacaff",
+      borderHoverColor: "#29008a",
+    };
+
     this.wrapper = document.createElement("a");
     this.wrapper.classList.add("wrapper");
 
     this.span = document.createElement("span");
     this.span.classList.add("content");
 
-    // Create some CSS to apply to the shadow dom
-    const style = document.createElement("style");
-
-    style.textContent = `
-      .wrapper {
-        border: 2px solid #ababab;
-        background-color: #ffffff;
-        color: black;
-        position: absolute;
-        width: 20em;
-        height: 2.5em;
-        display: flex;
-        text-decoration: none;
-        font-weight: bold;
-        opacity: 1;
-      }
-
-      .wrapper:hover {
-        background-color: #dacaff;
-        border-top: 2px solid #29008a;
-        border-bottom: 2px solid #29008a;
-      }
-
-      .right {
-        top: 4em;
-        right: -5em;
-        transform: rotate(45deg);
-      }
-
-      .left {
-        top: 4em;
-        left: -5em;
-        transform: rotate(315deg);
-      }
-
-      .content {
-        font-family: Arial;
-        margin: 0 auto;
-        display: flex;
-        align-items: center;
-      }
-    `;
-
+    const style = this.initStyleElement(this._theme);
     shadow.appendChild(style);
-
     this.wrapper.appendChild(this.span);
     shadow.appendChild(this.wrapper);
   }
@@ -69,6 +34,77 @@ class Ribbon extends HTMLElement {
     const href = this.getAttribute("href");
     this.wrapper.setAttribute("href", href);
     this.wrapper.setAttribute("target", "_blank");
+  }
+
+  get theme() {
+    return this._theme;
+  }
+
+  set theme(newVal) {
+    const newTheme = Object.assign(this._theme, newVal);
+    this._theme = newTheme;
+    this.updateStyle(this);
+  }
+
+  updateStyle(elem) {
+    const shadow = elem.shadowRoot;
+    shadow.querySelector("style").textContent = this.getStyleContent(
+      this.theme
+    );
+  }
+
+  initStyleElement(theme) {
+    // Create some CSS to apply to the shadow dom
+    const style = document.createElement("style");
+    style.textContent = this.getStyleContent(theme);
+    return style;
+  }
+
+  getStyleContent({
+    borderColor,
+    backgroundColor,
+    backgroundHoverColor,
+    borderHoverColor,
+  }) {
+    return `
+.wrapper {
+  border: 2px solid ${borderColor};
+  background-color: ${backgroundColor};
+  color: black;
+  position: absolute;
+  width: 20em;
+  height: 2.5em;
+  display: flex;
+  text-decoration: none;
+  font-weight: bold;
+  opacity: 1;
+}
+
+.wrapper:hover {
+  background-color: ${backgroundHoverColor};
+  border-top: 2px solid ${borderHoverColor};
+  border-bottom: 2px solid ${borderHoverColor};
+}
+
+.right {
+  top: 4em;
+  right: -5em;
+  transform: rotate(45deg);
+}
+
+.left {
+  top: 4em;
+  left: -5em;
+  transform: rotate(315deg);
+}
+
+.content {
+  font-family: Arial;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+}
+`;
   }
 }
 
